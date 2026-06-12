7ee4a3171e715607712ec3b692c5f1022a203992
Sonu Kumar <kumarsk002005@gmail.com>
2026-06-13 01:52:24 +0530

    fix dist folder

--- moodify Project/Backend/src/app.js
+++ moodify Project/Backend/src/app.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 require("./config/cache"); // Import Redis connection
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const path = require("path")
 // const corsOptions = {
 //     origin: 'http://localhost:5173', // Frontend URL
 //     credentials: true, // Allow cookies to be sent
@@ -23,4 +24,12 @@ const SongRouter = require("./routes/song.routes");
 // Using Routes
 app.use("/api/auth", AuthRouter);
 app.use("/api/song", SongRouter);
+
+
+//dist setup
+app.use(express.static(path.join(__dirname, "../dist")));
+
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, "../dist", "index.html"));
+});
 module.exports = app;
